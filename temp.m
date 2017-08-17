853d8a084cfa99fabc57f90cfc7d9e242914c449
pierrejacques <pierrejacques@126.com>
2017-08-18 00:25:07 +0800

    r跑完了结果

--- temp.m
+++ temp.m
@@ -2,13 +2,13 @@ discCell3 = {};
 weightlessCell3 = {};
 heatCell3 = {};
 
-for i = 1:90
+for i = 1:120
     discCell3{i} = discCell{i};
     heatCell3{i} = heatCell{i};
     weightlessCell3{i} = weightlessCell{i};
 end
-for i = 93:120
-    discCell3{i} = discCell2{i};
-    heatCell3{i} = heatCell2{i};
-    weightlessCell3{i} = weightlessCell2{i};
+for i = 120:198
+    discCell3{i} = discCell0{i};
+    heatCell3{i} = heatCell0{i};
+    weightlessCell3{i} = weightlessCell0{i};
 end
