df8524e7bd44863ac5002eac813fbbcd5c00db2e
金辰浩 <jinchenhao681@C17Q3305FVH5.lan>
2017-08-17 16:43:09 +0800

    enumerateR.m

---
+++ enumerateR.m
@@ -0,0 +1,21 @@
+function [heatCell, weightlessCell, discCell] = enumerateR(minR, maxR)
+% the r here refers to 3*sigma in Gauss or radius in Disk
+if nargin == 0
+    minR = 1;
+    maxR = 90;
+else if nargin == 1
+        maxR = 90;
+    end
+end
+
+heatCell = {};
+weightCell = {};
+discCell = {};
+
+for r = minR:maxR
+    if mod(r, 3) == 0
+        %À„gauss
+        [heatCell{r}, weightCell{r}, discCell{r}] = spaceEntropyGlobal(getKernel('gaussian', r/3));
+    end
+    
+end
