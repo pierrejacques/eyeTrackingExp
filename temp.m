6c1243f003bb94a8347e0e90d18c12bdb1905333
pierrejacques <pierrejacques@126.com>
2017-08-18 14:44:18 +0800

    merge data to r=300

--- temp.m
+++ temp.m
@@ -2,12 +2,12 @@ discCell3 = {};
 weightlessCell3 = {};
 heatCell3 = {};
 
-for i = 1:198
+for i = 1:120
     discCell3{i} = discCell{i};
     heatCell3{i} = heatCell{i};
     weightlessCell3{i} = weightlessCell{i};
 end
-for i = 198:500
+for i = 120:300
     discCell3{i} = discCell0{i};
     heatCell3{i} = heatCell0{i};
     weightlessCell3{i} = weightlessCell0{i};
