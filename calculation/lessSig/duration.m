9802f356c551db59142508b78c3bf31555e4c558
Pierrejacques from ASUS fx50j <pierrejacques@126.com>
2017-09-11 00:01:19 +0800

    figures completed

--- calculation/lessSig/duration.m
+++ calculation/lessSig/duration.m
@@ -1,26 +1,20 @@
-function [all_mean, mean_mean, mean_max, mean_min] = duration()
+function [all_mean, mean_mat, max_mat, min_mat] = duration()
 load ../data
 
 all_mean = zeros(1, max(pics));
-mean_mean = all_mean;
-mean_max = all_mean;
-mean_min = all_mean;
+mean_mat = zeros(max(users), max(pics));
+max_mat = mean_mat;
+min_mat = mean_mat;
 
 for pic = pics
     v = [];
-    mean_mat = zeros(1, max(users));
-    max_mat = mean_mat;
-    min_mat = mean_mat;
     for user = users
         dur_mat = fixations{user, pic}(:, 2);
-        max_mat(user) = max(dur_mat);
-        min_mat(user) = min(dur_mat);
-        mean_mat(user) = mean(dur_mat);
+        max_mat(user, pic) = max(dur_mat);
+        min_mat(user, pic) = min(dur_mat);
+        mean_mat(user, pic) = mean(dur_mat);
         v = [v;dur_mat];
     end
     all_mean(pic) = mean(v);
-    mean_mean(pic) = mean(mean_mat);
-    mean_max(pic) = mean(max_mat);
-    mean_min(pic) = mean(min_mat);
 end
 
--- calculation/showT.m
+++ calculation/showT.m
@@ -1,4 +1,4 @@
-function [rmat, rmat_1] = showT()
+function [rmat] = showT()
 load data
 load enumT
 weightless_1 = {};
@@ -9,31 +9,44 @@ for pic = pics
     end
     pre = pre / 30;
     weightless_1{pic} = weightless{pic}./pre;
+    end
+
+figure(1);
+hold on;
+for pic = pics
+    if category(pic) == 0
+        plot(weightless{pic}, 'r-');
+    end
+    if category(pic) == 1
+        plot(weightless{pic}, 'b-');
+    end
 end
 
-% for pic = pics
-%     if category(pic) == 0
-%         plot(weightless{pic}, 'r-');
-%     end
-%     if category(pic) == 1
-%         plot(weightless{pic}, 'b-');
-%     end
-% end
+figure(2);
+hold on;
+for pic = pics
+    if category(pic) == 0
+        plot(weightless_1{pic}, 'r-');
+    end
+    if category(pic) == 1
+        plot(weightless_1{pic}, 'b-');
+    end
+end
 
+figure(3);
 rmat = zeros(3000, 1);
-% rmat_1 = zeros(3000, 1);
-figure();
+rmat_1 = zeros(3000, 1);
 for t = 1:3000
     entMat = [];
-%     entMat_1 = [];
+    entMat_1 = [];
     for pic = pics
         entMat(pic) = weightless{pic}(t);
-%         entMat_1(pic) = weightless_1{pic}(t); %#ok<AGROW>
+        entMat_1(pic) = weightless_1{pic}(t); %#ok<AGROW>
     end
     rmat(t) = corr(score(pics)', entMat(pics)');
-%     rmat_1(t) = corr(score(pics)', entMat_1(pics)');
+    rmat_1(t) = corr(score(pics)', entMat_1(pics)');
 end
 plot(rmat);
 hold on
-% plot(rmat_1, 'r');
+plot(rmat_1, 'r');
     
