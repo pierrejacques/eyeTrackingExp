cae3f6f5ff6a6f6fefa49942bc99a7da24853d70
pierrejacques from LAB <pierrejacques@126.com>
2017-09-20 09:11:36 +0800

    reconstruct enumT.mat & showT

--- calculation/showT.m
+++ calculation/showT.m
@@ -1,15 +1,6 @@
 function [rmat] = showT(data)
 load data
 load enumT
-% weightless_1 = {};
-% for pic = pics
-%     pre = zeros(1, 3000);
-%     for user = users
-%         pre = pre + singleByT{user, pic};
-%     end
-%     pre = pre / 30;
-%     weightless_1{pic} = weightless{pic}./pre;
-% end
 
 figure(1);
 hold on;
@@ -21,17 +12,6 @@ for pic = pics
         plot(data{pic}(2:3000), 'b-');
     end
 end
-% 
-% figure(2);
-% hold on;
-% for pic = pics
-%     if category(pic) == 0
-%         plot(weightless_1{pic}, 'r-');
-%     end
-%     if category(pic) == 1
-%         plot(weightless_1{pic}, 'b-');
-%     end
-% end
 
 figure(2);
 rmat = zeros(3000, 1);
